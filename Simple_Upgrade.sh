626f8ba9ac67acff62d7ba969f7401e5ccff9867
PlexusCoin <40145647+PlexusCoin@users.noreply.github.com>
2018-06-19 16:53:06 +0700

    Update Simple_Upgrade.sh

--- Simple_Upgrade.sh
+++ Simple_Upgrade.sh
@@ -4,7 +4,8 @@ CONFIGFOLDER='/root/.plexus'
 COIN_DAEMON='plexusd'
 COIN_CLI='plexus-cli'
 COIN_PATH='/usr/local/bin/'
-COIN_TGZ='https://github.com/altcuim/Plexus/archive/v2.0.4/linux_v2.0.4.tar.gz'
+COIN_TGZ='https://github.com/PlexusCoin/Plexus/releases/download/v2.0.4/linux_v2.0.4.tar.gz'
+COIN_ZIP=$(echo $COIN_TGZ | awk -F'/' '{print $NF}')
 
 COIN_NAME='Plexus'
 COIN_PORT=31001
