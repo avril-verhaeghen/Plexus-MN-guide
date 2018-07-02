77b44dadae59a77ca67244d913cfab4222d35351
PlexusCoin <40145647+PlexusCoin@users.noreply.github.com>
2018-07-02 10:37:47 +0700

    Require MN Private Key

--- PlexusMN_install.sh
+++ PlexusMN_install.sh
@@ -120,25 +120,14 @@ EOF
 }
 
 function create_key() {
-  echo -e "Enter your ${RED}$COIN_NAME Masternode Private Key${NC}. Leave it blank to generate a new ${RED}Masternode Private Key${NC} for you:"
+  echo -e "Enter your ${RED}$COIN_NAME Masternode Private Key${NC}:"
   read -e COINKEY
-  if [[ -z "$COINKEY" ]]; then
-  $COIN_PATH$COIN_DAEMON -daemon
-  sleep 30
-  if [ -z "$(ps axo cmd:100 | grep $COIN_DAEMON)" ]; then
-   echo -e "${RED}$COIN_NAME server couldn not start. Check /var/log/syslog for errors.{$NC}"
-   exit 1
-  fi
-  COINKEY=$($COIN_PATH$COIN_CLI masternode genkey)
-  if [ "$?" -gt "0" ];
-    then
-    echo -e "${RED}Wallet not fully loaded. Let us wait and try again to generate the Private Key${NC}"
-    sleep 30
-    COINKEY=$($COIN_PATH$COIN_CLI masternode genkey)
-  fi
-  $COIN_PATH$COIN_CLI stop
-fi
-clear
+  while [[ -z "$COINKEY" ]]
+do
+        echo -e "Enter your ${RED}$COIN_NAME Masternode Private Key${NC}:"
+         read -e COINKEY
+done
+        printf "Your Masternode Private Key: %s " "$COINKEY"
 }
 
 function update_config() {
