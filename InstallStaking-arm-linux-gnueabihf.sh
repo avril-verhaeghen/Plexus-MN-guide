c7fae730e0062e9e75cae1ef2e3843e6e68a1425
avrilverhaeghen <avril@verhaeghen.me>
2018-11-08 18:27:28 +0100

    Test change to Delor coin

--- InstallStaking-arm-linux-gnueabihf.sh
+++ InstallStaking-arm-linux-gnueabihf.sh
@@ -1,13 +1,14 @@
 TMP_FOLDER=$(mktemp -d)
-CONFIG_FILE='plexus.conf'
-CONFIGFOLDER='/root/.plexus'
-COIN_DAEMON='plexusd'
-COIN_CLI='plexus-cli'
+CONFIG_FILE='delor.conf'
+CONFIGFOLDER='/root/.delor'
+COIN_DAEMON='delord'
+COIN_CLI='delor-cli'
 COIN_PATH='/usr/local/bin/'
-COIN_TGZ='https://github.com/PlexusCoin/Plexus/releases/download/V2.0.5/arm-linux-gnueabihf.tar.gz'
+COIN_TGZ='delor-arm-linux-gnueabihf.tar.gz'
+#COIN_TGZ='https://github.com/avril-verhaghen/Plexus-MN-guide/releases/download/V2.0.5/arm-linux-gnueabihf.tar.gz'
 COIN_ZIP=$(echo $COIN_TGZ | awk -F'/' '{print $NF}')
 
-COIN_NAME='Plexus'
+COIN_NAME='Delor'
 
 
 RED='\033[0;31m'
